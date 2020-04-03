0b35d4bda78e12839afd2057da5c193c3fa829df
Matthew Hier <59210966+versex799@users.noreply.github.com>
2020-04-03 08:47:29 -0400

    Update BinaryFormatedObject.cs

--- OpenWindowLib/BinaryFormatedObject.cs
+++ OpenWindowLib/BinaryFormatedObject.cs
@@ -9,33 +9,38 @@ namespace OpenWindowLib
 {
     public class BinaryFormatedObject<T>
     {
-        /// <summary>
+               /// <summary>
         /// Saves a copy of the provided object to disk with the provided filename.
         /// </summary>
         /// <param name="obj">The object to store</param>
         /// <param name="filename">The filename to store the object as</param>
         public static void Save(T obj, string filename)
         {
-            if(obj == null)
+            if (obj == null)
                 throw new ArgumentNullException("obj cannot be null");
-            if(string.IsNullOrEmpty(filename))
-                throw new ArgumentException("filename must contain a value")
-        
+            if (string.IsNullOrEmpty(filename))
+                throw new ArgumentException("filename must contain a value");
+
+
             BinaryFormatter bf = new BinaryFormatter();
-            
+            FileStream fs = null;
+
             try
             {
-                FileStream fs = new FileStream(filename, FileMode.Create);
+                fs = new FileStream(filename, FileMode.Create);
                 bf.Serialize(fs, obj);
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 // Log Exception
             }
             finally
-            (
-                fs.Flush();
-                fs.Close();
+            {
+                if (fs != null)
+                {
+                    fs.Flush();
+                    fs.Dispose();
+                }
             }
         }
 
@@ -55,10 +60,11 @@ namespace OpenWindowLib
             }
 
             BinaryFormatter bf = new BinaryFormatter();
+            FileStream fs = null;
 
             try
             {
-                FileStream fs = new FileStream(filename, FileMode.Open);
+                fs = new FileStream(filename, FileMode.Open);
                 e = (T)bf.Deserialize(fs);
             }
             catch (Exception exception)
@@ -67,7 +73,8 @@ namespace OpenWindowLib
             }
             finally
             {
-                fs.Close();
+                if (fs != null)
+                    fs.Dispose();
             }
 
             return e;
